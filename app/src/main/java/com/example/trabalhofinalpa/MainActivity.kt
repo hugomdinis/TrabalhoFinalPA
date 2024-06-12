28564e222657c460640ff58d88a08d606968ad86
Hugo Dinis <105894705+hugomdinis@users.noreply.github.com>
2024-06-12 15:00:34 +0100

    Criado um preview para a ver a view DrinkScreen e  modificações na view para adicionar a quantiade e o valor por unidade

--- app/src/main/java/com/example/trabalhofinalpa/MainActivity.kt
+++ app/src/main/java/com/example/trabalhofinalpa/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,7 +19,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -101,9 +107,41 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
     @Composable
     fun DrinkScreen(drinkName: String) {
+        var quantity by remember { mutableStateOf("") }
+        var unitPrice by remember { mutableStateOf("") }
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.beer_mug), // Substitua pela imagem correspondente ao drink
+                contentDescription = drinkName,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Quantity of $drinkName")
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = quantity,
+                onValueChange = { quantity = it },
+                label = { Text(text = "Quantity") },
+                modifier = Modifier.width(200.dp)
+                    .padding(bottom = 20.dp)
+            )
+            TextField(
+                value = unitPrice,
+                onValueChange = { unitPrice = it },
+                label = { Text(text = "Unit Price") },
+                modifier = Modifier.width(200.dp)
+            )
+        }
     }
 
     @Preview(showBackground = true)
@@ -111,6 +149,14 @@ class MainActivity : ComponentActivity() {
     fun NightOutPreview() {
         TrabalhoFinalPATheme {
             NightOutLayout(rememberNavController())
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DrinkScreenPreview() {
+        TrabalhoFinalPATheme {
+            DrinkScreen(drinkName = "Beer")
         }
     }
 }
