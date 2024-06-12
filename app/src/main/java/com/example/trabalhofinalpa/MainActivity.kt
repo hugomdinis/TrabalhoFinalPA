a6b4fbe7e3e421e8fb1945abd2a0297db965c9c0
Hugo Dinis <105894705+hugomdinis@users.noreply.github.com>
2024-06-12 11:03:12 +0100

    Adicionar LazyColumn, imagens e botões para o inicio da aplicação

--- app/src/main/java/com/example/trabalhofinalpa/MainActivity.kt
+++ app/src/main/java/com/example/trabalhofinalpa/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.trabalhofinalpa.ui.theme.TrabalhoFinalPATheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrabalhoFinalPATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    NightOutLayout()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+    @Composable
+    fun NightOutLayout() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabalhoFinalPATheme {
-        Greeting("Android")
+        val drinks = listOf(
+            Drink("Beer", R.drawable.cocktail),
+            Drink("Cocktail", R.drawable.cocktail),
+            Drink("Wine", R.drawable.wine),
+        )
+
+        LazyColumn (
+            modifier = Modifier
+                .padding(40.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            items(drinks) {drink ->
+                DrinkButton(drink)
+            }
+        }
+    }
+
+    @Composable
+    fun DrinkButton(drink: Drink) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Image(
+                    painter = painterResource(id = drink.imageRes),
+                    contentDescription = drink.name,
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = drink.name)
+            }
+        }
+    }
+
+    data class Drink(val name: String, val imageRes: Int)
+
+    @Preview(showBackground = true)
+    @Composable
+    fun NightOutPreview() {
+        TrabalhoFinalPATheme {
+            NightOutLayout()
+        }
     }
 }
