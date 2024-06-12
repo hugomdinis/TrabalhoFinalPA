ea423ca4f26dc9acfaa6cc398ec8dce3577b50a9
Hugo Dinis <105894705+hugomdinis@users.noreply.github.com>
2024-06-12 12:37:40 +0100

    Adicionar Imagens de bebidas

--- app/src/main/java/com/example/trabalhofinalpa/Data/Drink.kt
+++ app/src/main/java/com/example/trabalhofinalpa/Data/Drink.kt
@@ -8,23 +8,20 @@ data class Drink(val name: String, val imageRes: Int){
     // foi usado um companion object para o ser de facil acesso e ficar interligado com a data class
     companion object {
         val drinks = listOf(
-            Drink("Beer", R.drawable.beer),
-            Drink("Cocktail", R.drawable.cocktail),
-            Drink("Wine", R.drawable.wine),
-            /*Drink("Martini", R.drawable.martini),
+            Drink("Beer", R.drawable.beer_mug),
+            Drink("Martini", R.drawable.martini),
+            Drink("Wine", R.drawable.copodevinho),
             Drink("Margarita", R.drawable.margarita),
             Drink("Whiskey", R.drawable.whiskey),
             Drink("Mojito", R.drawable.mojito),
-            Drink("Tequila", R.drawable.tequila),
-            Drink("Gin Tonic", R.drawable.gin_tonic),
+            Drink("Tequila", R.drawable.dosedetequila),
+            Drink("Gin Tonic", R.drawable.tonica),
             Drink("Rum", R.drawable.rum),
             Drink("Vodka", R.drawable.vodka),
-            Drink("Champagne", R.drawable.champagne),
+            Drink("Champagne", R.drawable.champanhe),
             Drink("Sangria", R.drawable.sangria),
-            Drink("Cider", R.drawable.cider),
-            Drink("Bloody Mary", R.drawable.bloody_mary),
-            Drink("Brandy", R.drawable.brandy),
-            Drink("Absinthe", R.drawable.absinthe)*/
+            Drink("Water", R.drawable.garrafadeagua),
+            Drink("Absinthe", R.drawable.absinto)
         )
     }
 }
